f0f167e83cc29532a9177849b3f69ff93cca6d10
Konstantin Nesterov <wasd171@gmail.com>
2022-07-20 03:51:02 -0700

    Upgrade mikro-orm to 5.2.3 and try JOINED strategy

--- mikro-orm.config.js
+++ mikro-orm.config.js
@@ -1,6 +1,9 @@
+const { LoadStrategy } = require("@mikro-orm/core");
+
 module.exports = {
   entities: ["./build/entities"],
   dbName: ":memory:",
   type: "sqlite",
   debug: true,
+  loadStrategy: LoadStrategy.JOINED,
 };
